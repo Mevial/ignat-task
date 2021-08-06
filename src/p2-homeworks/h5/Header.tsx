56bfb75579ae0fb5fa95df57467419869dd321e0
Max <61512470+Mevial@users.noreply.github.com>
2021-08-06 17:54:27 +0300

    HW 5 add GH-PAGES and navLink + route

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import styles from './Header.module.css'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={styles.header}>
+           <NavLink to={PATH.PRE_JUNIOR} className={styles.link} activeClassName={styles.active}>pre-junior</NavLink>
+           <NavLink to={PATH.JUNIOR} className={styles.link} activeClassName={styles.active}>junior</NavLink>
+           <NavLink to={PATH.JUNIOR_PLUS} className={styles.link} activeClassName={styles.active}>junior+</NavLink>
+            <div className={styles.block}/>
 
         </div>
     )
