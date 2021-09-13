328a652364bb99d69a5cfbc3f8818e5e723693a8
Max <61512470+Mevial@users.noreply.github.com>
2021-09-13 22:03:48 +0300

    HW 11  SuperDoubleRange

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,18 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import {SuperDoubleRange} from "./common/c8-SuperDoubleRange/SuperDoubleRange";
 
 function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
+
+    const qwe = (value: number | number[]) => {
+        if (typeof value !== 'number') {
+            setValue1(value[0])
+            setValue2(value[1])
+        }
+
+    }
 
     return (
         <div>
@@ -14,17 +22,20 @@ function HW11() {
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
+                <SuperRange onChangeRange={setValue1}
+                            value={value1}
                     // сделать так чтоб value1 изменялось
                 />
             </div>
 
             <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
+                <SuperDoubleRange onChangeRange={qwe}
+                                  value={[value1, value2]}
+                                  disabled={false}
+                                  min={0}
+                                  max={100}
                     // сделать так чтоб value1 и value2 изменялось
                 />
-                <span>{value2}</span>
             </div>
 
             <hr/>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,82 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
+
+const useStyles = makeStyles({
+    root: {
+        width: 300,
+        margin: 20
+    },
+});
+
+function valuetext(value: number) {
+    return `${value}`;
+}
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
+    onChangeRange?: (value: number | number[]) => void
     value?: [number, number]
+    disabled: boolean
+    min: number
+    max: number
     // min, max, step, disable, ...
 }
 
-const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange, value,
-        // min, max, step, disable, ...
-    }
-) => {
-    // сделать самому, можно подключать библиотеки
+
+export const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (props) => {
+    const classes = useStyles();
+
+    const handleChange = (event: any, newValue: number | number[]) => {
+        console.log(newValue)
+        props.onChangeRange && props.onChangeRange(newValue);
+
+    };
 
     return (
-        <>
-            DoubleRange
-        </>
-    )
+        <div className={classes.root}>
+            <Typography id="range-slider" gutterBottom>
+                <div>
+                    range-mui
+                </div>
+            </Typography>
+            <Slider
+                min={0}
+                max={100}
+                disabled={props.disabled}
+                value={props.value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                aria-labelledby="range-slider"
+                getAriaValueText={valuetext}
+            />
+        </div>
+    );
 }
 
-export default SuperDoubleRange
+
+// import React from 'react'
+//
+//
+// type SuperDoubleRangePropsType = {
+//     onChangeRange?: (value: [number, number]) => void
+//     value?: [number, number]
+//     // min, max, step, disable, ...
+// }
+//
+// const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
+//     {
+//         onChangeRange, value,
+//         // min, max, step, disable, ...
+//     }
+// ) => {
+//
+//
+//     return (
+//         <>
+//             DoubleRange
+//         </>
+//     )
+// }
+//
+// export default SuperDoubleRange
